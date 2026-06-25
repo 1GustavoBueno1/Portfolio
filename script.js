6be45e9f1ed01c7fedadb133181aac2fcbdedea6
unknown <Gustavo.silva123913@gmail.com>
2026-06-25 00:28:28 -0300

    Adicionando animaçao de aparecimento dos elementos

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const titulo = document.querySelector(".botao");
-const fundo = document.body;
-let ligado_escuro = false;
-fundo.addEventListener("click", function(){
-    if (ligado_escuro === false){
-        titulo.textContent = "Modo Escuro";
-        fundo.style.background = "black";
+console.log("script.js carregou!");
+const elementos = document.querySelectorAll(".Animar");
+const observador = new IntersectionObserver(function(entradas)
+{
+    entradas.forEach(function(entrada)
+{   if (entrada.isIntersecting === true) {
+        entrada.target.classList.add("Visivel")
+        };
 
-        ligado_escuro = true
-    } else {
-        titulo.textContent = "Modo claro";
-        fundo.style.background = "#0d1b2a";
-        ligado_escuro = false;
-    };
+    });
+});
+elementos.forEach(function(elemento)
+{
+    observador.observe(elemento)
 });
